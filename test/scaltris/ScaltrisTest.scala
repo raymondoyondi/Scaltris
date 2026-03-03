cdc7a30c24e44a954364defa95252729170d451b
Raymond Oyondi <raymondoyondi@gmail.com>
2026-03-03 10:38:39 -0600

    Create ScaltrisTest.scala

---
+++ test/scaltris/ScaltrisTest.scala
@@ -0,0 +1,11 @@
+package scaltris
+
+import org.scalatest._
+
+/**
+  * Run this spec to test that your Scaltris class works properly.
+  *
+  * @author raymondoyondi
+  */
+class ScaltrisTest extends FlatSpec with Matchers {
+}
